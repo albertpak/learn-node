38a9489981d580dbb021313916388748b66fc203
Albert Pak <al@albert-pak.com>
2017-06-11 21:39:02 -0500

    changing up create and edit flow for stores

---
+++ controllers/storeController.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => {
+    res.render('index');
+};
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {
+        title: 'Add Store'
+    });
+};
+
+exports.createStore = async(req, res) => {
+    const store = await (new Store(req.body)).save();
+
+    req.flash('success', `Successfully created ${store.name}.`)
+
+    res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async(req, res) => {
+    const stores = await Store.find();
+    res.render('stores', {
+        title: 'Stores',
+        stores,
+    });
+};
+
+exports.editStore = async(req, res) => {
+    const store = await Store.findOne({ _id: req.params.id });
+
+    res.render('editStore', {
+        title: 'Edit Store',
+        store
+    });
+};
+
+exports.updateStore = async(req, res) => {
+    const store = await Store.findOneAndUpdate({ _id: req.params.id },
+        req.body, {
+            new: true, // return new store instead of the old one
+            runValidators: true
+        }
+    ).exec();
+
+    req.flash('success', `Successfully updated!`);
+    res.redirect(`/stores/${store.id}/edit`);
+};
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController');
+
+const { catchErrors } = require('../handlers/errorHandlers');
+
+// Do work here
+router.get('/', storeController.getStores);
+router.get('/stores', storeController.getStores);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+
+module.exports = router;
