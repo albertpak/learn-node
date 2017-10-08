339b831505cba1c5717bf19b0662ab2e19856dcc
Albert Pak <al@albert-pak.com>
2017-10-08 14:29:28 -0500

    wip - user model, user validations

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid Email Address'],
+    required: 'Please Supply an Email Address',
+  },
+  name: {
+    type: String,
+    required: 'Please Supply a Name',
+    trim: true,
+  },
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
